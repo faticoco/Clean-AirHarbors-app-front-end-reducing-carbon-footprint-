c9dd6d03bf7574a150ffdcdfd4bdb91a8afb4cff
fatima <fb71459@gmail.com>
2022-11-25 19:44:56 +0500

    added footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,9 +5,28 @@
 	import Tailwind from './Tailwind.svelte';
 </script>
 
+<style>
+	.ContactCards {
+	display: inline-blocks;
+	margin-top: 1rem;
+	padding: 1rem;
+	border: 2px red;
+	}
+
+	.body{
+	background-color:white;
+	text-align: center;
+	}
+</style>
+
 <Header />
+<div class="body" >
+	<h1 class="text-5xl font-extrabold dark:text-black" style="color:black">
+		Carbon Info
+	</h1>
 
-<Hero />
+</div>
 
-<Footer/>
-
+<div class="ContactCards">
+<Footer />
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,59 +1,80 @@
 const config = {
-  content: [
-    "./src/**/*.{html,js,svelte,ts}",
-    "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
-  ],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+	],
 
-  theme: {
-    extend: {
-      colors: {
-        primary: { "50": "#fdf0e8", "100": "#f9d2b9", "200": "#f4b58a", "300": "#f0975b", "400": "#ec792d", "500": "#d26013", "600": "#a44a0f", "700": "#75350b", "800": "#462006", "900": "#170b02" }
-      }
-    },
-    fontFamily: {
-      'body': [
-        'Montserrat',
-        'ui-sans-serif',
-        'system-ui',
-        '-apple-system',
-        'system-ui',
-        'Segoe UI',
-        'Roboto',
-        'Helvetica Neue',
-        'Arial',
-        'Noto Sans',
-        'sans-serif',
-        'Apple Color Emoji',
-        'Segoe UI Emoji',
-        'Segoe UI Symbol',
-        'Noto Color Emoji'
-      ],
-      'sans': [
-        'Montserrat',
-        'ui-sans-serif',
-        'system-ui',
-        '-apple-system',
-        'system-ui',
-        'Segoe UI',
-        'Roboto',
-        'Helvetica Neue',
-        'Arial',
-        'Noto Sans',
-        'sans-serif',
-        'Apple Color Emoji',
-        'Segoe UI Emoji',
-        'Segoe UI Symbol',
-        'Noto Color Emoji'
-      ]
-    }
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					50: '#fdf0e8',
+					100: '#f9d2b9',
+					200: '#f4b58a',
+					300: '#f0975b',
+					400: '#ec792d',
+					500: '#d26013',
+					600: '#a44a0f',
+					700: '#75350b',
+					800: '#462006',
+					900: '#170b02'
+				}
+			}
+		},
+		 daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
   },
+		fontFamily: {
+			body: [
+				'Montserrat',
+				'ui-sans-serif',
+				'system-ui',
+				'-apple-system',
+				'system-ui',
+				'Segoe UI',
+				'Roboto',
+				'Helvetica Neue',
+				'Arial',
+				'Noto Sans',
+				'sans-serif',
+				'Apple Color Emoji',
+				'Segoe UI Emoji',
+				'Segoe UI Symbol',
+				'Noto Color Emoji'
+			],
+			sans: [
+				'Montserrat',
+				'ui-sans-serif',
+				'system-ui',
+				'-apple-system',
+				'system-ui',
+				'Segoe UI',
+				'Roboto',
+				'Helvetica Neue',
+				'Arial',
+				'Noto Sans',
+				'sans-serif',
+				'Apple Color Emoji',
+				'Segoe UI Emoji',
+				'Segoe UI Symbol',
+				'Noto Color Emoji'
+			]
 
-  plugins: [
-    require('flowbite/plugin')
-  ],
-  darkMode: 'class',
+		}
+	},
+
+	plugins: [require('flowbite/plugin')],
+	plugins: [require("daisyui")],
+	darkMode: 'class'
+
 };
-
 
 
 module.exports = config;
