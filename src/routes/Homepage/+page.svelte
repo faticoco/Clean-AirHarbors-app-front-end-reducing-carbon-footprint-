4fbe89b2385096729bd1a713ab4b658bc275f24d
Noman Mustafa <mnmustafa1109@gmail.com>
2022-11-26 21:24:12 +0500

    fixed reminder

--- src/routes/Homepage/+page.svelte
+++ src/routes/Homepage/+page.svelte
@@ -7,44 +7,45 @@
 	import carousel2 from '$lib/assets/carousel2.png';
 	import carousel3 from '$lib/assets/carousel3.png';
 	import goals from '$lib/assets/goals.png';
-	import main from '$lib/assets/main_img.jpg';
 </script>
 
 <Header />
 <div class="text-sm breadcrumbs">
-    <ul>
-        <li><a>Home</a></li>
-    </ul>
+	<ul>
+		<li><a>Home</a></li>
+	</ul>
 </div>
 
-<div class="hero min-h-screen bg-white">
-    <div class="hero-content text-center">
-      <div class="max-w-xl">
-	  <img src="{main}" class="object-scale-down h-500 w-full" />
-				<h1 class="text-5xl font-bold">Clean Hair Harbor</h1>
-				<p class="py-6">Clean Air Harbors. Make your environment clean</p>
-				<button class="btn btn-primary">Get Started</button>
-      </div>
-    </div>
-  </div>
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold">Clean Hair Harbour</h1>
+			<p class="py-6">
+				Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+				quasi. In deleniti eaque aut repudiandae et a id nisi.
+			</p>
+			<button class="btn btn-primary">Get Started</button>
+		</div>
+	</div>
+</div>
 
-<div class="carousel w-xl">
+<div class="carousel w-full">
 	<div id="slide1" class="carousel-item relative w-full">
-		<img src="{carousel1}"  class="object-cover h-100 w-full" />
+		<img src={carousel1} class="object-cover h-110 w-full" />
 		<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
 			<a href="#slide4" class="btn btn-circle">❮</a>
 			<a href="#slide2" class="btn btn-circle">❯</a>
 		</div>
 	</div>
 	<div id="slide2" class="carousel-item relative w-full">
-		<img src="{carousel2}"class="object-cover h-100 w-full" />
+		<img src={carousel2} class="object-cover h-110 w-full" />
 		<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
 			<a href="#slide1" class="btn btn-circle">❮</a>
 			<a href="#slide3" class="btn btn-circle">❯</a>
 		</div>
 	</div>
 	<div id="slide3" class="carousel-item relative w-full">
-		<img src="{carousel3}" class="object-scale-down h-100 w-full" />
+		<img src={carousel3} class="object-scale-down h-110 w-full" />
 		<div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
 			<a href="#slide2" class="btn btn-circle">❮</a>
 			<a href="#slide4" class="btn btn-circle">❯</a>
@@ -52,9 +53,7 @@
 	</div>
 </div>
 
-
-
-<div class="hero min-h-screen  bg-white">
+<div class="hero min-h-screen bg-base-200">
 	<div class="hero-content flex-col lg:flex-row-reverse">
 		<img src={goals} class="max-w-sm rounded-lg shadow-2xl" />
 		<div>
@@ -72,26 +71,20 @@
 <br />
 <br />
 
-<div class="preview">
-	<h1 class="text-2xl font-extrabold dark:text-black" style="color:black">Reminder Preview</h1>
 
-	<div>
-		<p>
-			Subscribe to our App to avail the reminders feature <br />
+
+<div class="hero ">
+	<div class="hero-content text-center text-neutral-content">
+		<div class="max-w-md">
+			<h1 class="mb-5 text-5xl font-bold" style="color:black">Reminder Preview</h1>
+			<p class="mb-5" style="color:black">
+				Subscribe to our App to avail the reminders feature <br />
 			Made to remind you of your daily routine tasks <br />
-			to reduce your carbon footprint <br />
-		</p>
+			to reduce your carbon footprint 
+			</p>
+			<button class="btn btn-primary" >Preview</button>
+		</div>
 	</div>
-
-	<button
-		class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800"
-	>
-		<span
-			class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0"
-		>
-			Preview
-		</span>
-	</button>
 </div>
 
 <div class="ContactCards">
