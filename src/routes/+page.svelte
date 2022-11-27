4c3c02bf8a1cfcd89bfb2d1651fb20336b5b02d4
Noman Mustafa <mnmustafa1109@gmail.com>
2022-11-28 01:19:50 +0500

    added modal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -70,3 +70,30 @@
 		/>
 	</div>
 </div>
+
+<!-- The button to open modal -->
+<label for="my-modal" class="btn">open modal</label>
+
+<!-- Put this part before </body> tag -->
+<input type="checkbox" id="my-modal" class="modal-toggle" />
+<div class="modal">
+  <div class="modal-box">
+    <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <div class="modal-action">
+      <label for="my-modal" class="btn">Yay!</label>
+    </div>
+  </div>
+</div>
+
+<label for="my-modal-2" class="btn btn-primary modal-button">open modal</label> 
+<input type="checkbox" id="my-modal-2" class="modal-toggle"> 
+<div class="modal">
+  <div class="modal-box">
+    <p>Enim dolorem dolorum omnis atque necessitatibus. Consequatur aut adipisci qui iusto illo eaque. Consequatur repudiandae et. Nulla ea quasi eligendi. Saepe velit autem minima.</p> 
+    <div class="modal-action">
+      <label for="my-modal-2" class="btn btn-primary">Accept</label> 
+      <label for="my-modal-2" class="btn">Close</label>
+    </div>
+  </div>
+</div>
